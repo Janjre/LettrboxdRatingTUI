ffbd1afa4cca33d9c8c8500791bc20c15c08c094
Janjre <136253210+Janjre@users.noreply.github.com>
2026-03-09 20:28:06 +0000

    Added serve thing

--- serve.py
+++ serve.py
@@ -3,7 +3,7 @@ from textual_serve.server import Server
 
 server = Server(
     "python main.py",
-    host="0.0.0.0",
+    host="localhost",
     port=int(os.environ.get("PORT", 8000))
 )
 server.serve()
